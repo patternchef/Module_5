6f0d280fc8760d2516de6cc638cc6c35df091be5
jamdon <jamdon@uw.edu>
2015-05-08 00:30:48 -0700

    Update truckModel.js
    update instructions

--- models/truckModel.js
+++ models/truckModel.js
@@ -1,4 +1,7 @@
 // below you'll find the schema you need to export your truckModel
+// when exporting the mongoose model you will create using this schema, 
+// name your model 'Truck' (see the example in the slide deck for 'Book')
+// this is to match the name of the collection the data was imported into
 
 // var foodTruckSchema = new Schema({
 // 	name: String,
