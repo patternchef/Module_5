caee35a322a9ebc663f08cf7e5ffaef34d5867d8
jamdon <jamdon@uw.edu>
2015-05-07 20:55:38 -0700

    Update truckRoutes.js
    additional help instructions added

--- routes/truckRoutes.js
+++ routes/truckRoutes.js
@@ -1,1 +1,3 @@
+// connect to mongodb in this module as this is where you'll be making creat/read/delete calls to your database
 // use 'mongodb://localhost/foodTruckAPI' for your mongoose connection string
+// remember this is a Node module
