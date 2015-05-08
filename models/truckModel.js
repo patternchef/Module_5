2c0aa6571a0b671d4440ba3a2eaf7f5ee1b5a7ac
James Donaldson <kinakuta@Jamess-MacBook-Air.local>
2015-05-07 20:01:48 -0700

    added schema for truckModel

---
+++ models/truckModel.js
@@ -0,0 +1,12 @@
+// below you'll find the schema you need to export your truckModel
+
+// var foodTruckSchema = new Schema({
+// 	name: String,
+// 	foodType: [String],
+// 	schedule: [String],
+// 	payment: [String],
+// 	description: String,
+// 	website: String,
+// 	Facebook: String,
+// 	Twitter: String
+// });
